6cefd84d0e39ffebd71d8414bbb260ca02c321ad
aleksey.ilin <ilinalekseys@gmail.com>
2018-12-08 13:03:01 +0300

    add redux, redux-form, redux-action, fis components

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import './Item.css';
 
-const Item = () => (
-  <div className="table">
-    <div className="thead">
-      <div className="num"></div>
-      <div className="task"></div>
-      <div className="plan"></div>
-      <div className="fact"></div>
-      <div className="percent"></div>
-      <div className="forecast"></div>
-    </div>
-  </div>
-);
-
-export default Item;
+export default class Item extends React.Component {
+  
+  removeTask = () => {
+    const { children } = this.props;
+    console.log(children)
+  };
+  
+  render() {
+    return (
+      <div className="item">
+        <div className="numTask">
+          <button className="newTask" onClick={this.removeTask}>-</button>
+          <div className="num">{ /*children*/ }</div>
+        </div>
+        <div className="task">Task { /*children*/ }</div>
+        <div className="plan">10</div>
+        <div className="fact">10</div>
+        <div className="percent">100 %</div>
+        <div className="forecast">0</div>
+      </div>
+    );
+  }
+};
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
 import './Table.css';
+import Item from './Item';
 
-const Table = () => (
-  <div className="table">
-    <div className="thead">
-      <div className="numTask">
-        <button className="newTask">+</button>
-        <div className="num">№</div>
+export default class Table extends React.Component {
+  state = { tasks: [] };
+
+  addTask = () => {
+    const { tasks } = this.state;
+    // console.log(tasks);
+    tasks.length === 0 ? this.setState({ tasks: [1] }) : this.setState({ tasks: [...tasks, tasks.length + 1] });
+    // console.log(tasks);
+  };
+
+  render() {
+    const { tasks } = this.state;
+    return (
+      <div className="table">
+        <div className="thead">
+          <div className="numTask">
+            <button className="newTask" onClick={this.addTask}>+</button>
+            <div className="num">№</div>
+          </div>
+          <div className="task">Task</div>
+          <div className="plan">Plan</div>
+          <div className="fact">Fact</div>
+          <div className="percent">%</div>
+          <div className="forecast">Forecast</div>
+        </div>
+        {tasks.map(task => <Item>{task}</Item>)}
       </div>
-      <div className="task">Task</div>
-      <div className="plan">Plan</div>
-      <div className="fact">Fact</div>
-      <div className="percent">%</div>
-      <div className="forecast">Forecast</div>
-    </div>
-  </div>
-);
-
-export default Table;
+    );
+  }
+}
