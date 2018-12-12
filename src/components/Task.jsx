146d5b4c40a7e048f39aa0f9941ce3e5a4e037aa
aleksey.ilin <ilinalekseys@gmail.com>
2018-12-12 10:06:25 +0300

    fix Task.jsx

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,38 +1,15 @@
-/* global document:true */
-/* eslint no-undef: "error" */
 import React from 'react';
 import './Task.css';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
 
 export default class Tasks extends React.Component {
-  state = { editing_state: false };
-
-  componentWillUnmount() {
-    document.removeEventListener('click', this.handleClickOutside, false);
-  }
-
-  componentDidMount() {
-    document.addEventListener('click', this.handleClickOutside, false);
-  }
-
-  handleClickOutside = ({ target: { className } }) => {
-    if (className !== 'task cell-editing') {
-      this.setState({ editing_state: false });
-    }
-  }
-
-  changeStateCell = () => {
-    this.setState({ editing_state: true });
-  }
-
   editTaskText = id => (e) => {
-    console.log(e.target.innerHTML);
+    // console.log(e.target.value);
     const { updateTaskText } = this.props;
-    updateTaskText({ id, text: e.target.innerHTML });
+    updateTaskText({ id, text: e.target.value });
     const enterCode = 13;
     if (e.keyCode === enterCode) {
-      this.setState({ editing_state: false });
       e.target.blur();
     }
   }
@@ -45,7 +22,6 @@ export default class Tasks extends React.Component {
     const { task } = this.props;
     const cellClass = cn({
       task: true,
-      'cell-editing': this.state.editing_state,
     });
     return (
       <div className="item">
@@ -53,14 +29,7 @@ export default class Tasks extends React.Component {
           <button className="newTask" onClick={this.removeTask(task.id)}>-</button>
           <div className="num">{task.id}</div>
         </div>
-        <div
-          className={cellClass}
-          contentEditable="true"
-          data-text="New task"
-          onKeyDown={this.editTaskText(task.id)}
-          onKeyUp={this.editTaskText(task.id)}
-          onClick={this.changeStateCell}>
-        </div>
+        <input className={cellClass} placeholder="New task" onKeyUp={this.editTaskText(task.id)}></input>
         <div className="plan">10</div>
         <div className="fact">10</div>
         <div className="percent">100 %</div>
