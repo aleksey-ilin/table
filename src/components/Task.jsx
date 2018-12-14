d3f2c3acf9d7d268d00949ae850af60957a841f9
aleksey.ilin <ilinalekseys@gmail.com>
2018-12-14 22:15:14 +0300

    add timer button

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import './Task.css';
 import PropTypes from 'prop-types';
 import { round } from 'lodash';
+import cn from 'classnames';
 
 export default class Tasks extends React.Component {
+  state = { timerButton: 'stop' };
+
   handleClickEnter = (e) => {
     const enterCode = 13;
     if (e.keyCode === enterCode) {
@@ -37,11 +40,24 @@ export default class Tasks extends React.Component {
 
   removeTask = id => () => this.props.removeTask({ id });
 
+  changeTimerState = (e) => {
+    e.preventDefault();
+    const { timerButton } = this.state;
+    const newTimerButton = timerButton === 'stop' ? 'start' : 'stop';
+    this.setState({ timerButton: newTimerButton });
+  }
+
   render() {
+    // console.log(this.state);
     const { task } = this.props;
     const { plan, fact, percent } = task;
+    const { timerButton } = this.state;
     const cond = (plan === 0 || fact === 0 || percent === 0 || plan === '' || fact === '' || percent === '');
     const necessary = cond ? null : round(((100 * fact / percent) - fact), 1);
+    const timerButtonName = timerButton === 'stop' ? 'Stop' : 'Start';
+    const timerButtonClass = cn({
+      [`timerButton ${timerButton}`]: true,
+    });
     return (
       <div className="item">
         <div className="numTask">
@@ -49,6 +65,9 @@ export default class Tasks extends React.Component {
           <div className="num">{task.id}</div>
         </div>
         <input className="task" placeholder="New task" onKeyUp={this.editTaskText(task.id)}></input>
+        <button className={timerButtonClass} onClick={this.changeTimerState}>
+          { timerButtonName }
+        </button>
         <input className="plan" placeholder="0" type="number" onKeyUp={this.editPlan(task.id)}></input>
         <input className="fact" placeholder="0" type="number" onKeyUp={this.editFact(task.id)}></input>
         <input className="percent" placeholder="0" type="number" onKeyUp={this.editPercent(task.id)}></input>
