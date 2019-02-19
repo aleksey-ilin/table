779773f02402ffc4619eb1bbcfd354c5d33422e1
aleksey.ilin <ilinalekseys@gmail.com>
2019-02-19 19:07:09 +0300

    fix properties

--- src/components/Properties.jsx
+++ src/components/Properties.jsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import styles from './Properties.module.css';
 
 export default class Properties extends React.Component {
   editDescription = ({ target: { value } }) => {
-    const { activeProperties } = this.props;
-    this.props.changeDescription({ id: activeProperties, description: value });
+    const { activeProperties, changeDescription } = this.props;
+    changeDescription({ id: activeProperties, description: value });
   };
+
+  close = () => this.props.changeActiveProperties('-1');
 
   render() {
     // console.log(this.props);
     const { tasks, activeProperties } = this.props;
-    if (this.props.activeProperties === '-1') {
+    if (activeProperties === '-1') {
       return null;
     }
     return (
-      <div className={styles.properties}>
+      <div className={styles.root}>
+        <div className={styles.header}>
+          <button className={styles.close} onClick={this.close}>
+            <FontAwesomeIcon icon={faTimes} />
+          </button>
+        </div>
         <p>Description</p>
-        <textarea cols="25" rows="10" onChange={this.editDescription} value={tasks[activeProperties].description}></textarea>
+        <textarea cols="25" rows="10" onChange={this.editDescription} value={tasks[activeProperties].description} placeholder='Add a more detailed description'></textarea>
       </div>
     );
   }
@@ -26,5 +35,6 @@ export default class Properties extends React.Component {
 Properties.propTypes = {
   activeProperties: PropTypes.string,
   changeDescription: PropTypes.func,
+  changeActiveProperties: PropTypes.func,
   tasks: PropTypes.object,
 };
